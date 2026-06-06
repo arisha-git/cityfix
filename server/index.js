b3fe08120c05d83a97e4f6d3fac6a0f104c3479f
Arisha Ali <khan.arisha.ali@gmail.com>
2026-06-07 00:46:32 +0300

    cors update

--- server/index.js
+++ server/index.js
@@ -1,18 +1,26 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const connectDB = require('./config/db')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
 
-const app = express()
-connectDB()
+const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }))
-app.use(express.json())
+connectDB();
 
-app.use('/api/auth',   require('./routes/auth'))
-app.use('/api/issues', require('./routes/issues'))
-app.use('/api/issues', require('./routes/comments'))
+app.use(cors());
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-)
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/issues', require('./routes/issues'));
+app.use('/api/issues', require('./routes/comments'));
+
+app.get('/', (req, res) => {
+  res.send('CityFix API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
